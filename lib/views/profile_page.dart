51e78f95b1d407528a8bb01bdcded5ffa330c208
Didira123 <ogeiddiego064@gmail.com>
2023-12-03 17:18:44 -0300

    Ações:ProfilePage sem cores certas; Status:feito;

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -20,78 +20,105 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       // backgroundColor: Brand.background,
       appBar: const AppTitle('Perfil'),
-      body: Padding(
-        padding: const EdgeInsets.all(Brand.padding * 3),
-        child: Column(
-          children: [
-            // avatar
-            /*  */ const Spacer(),
-            Image.asset(user.picture),
-            /*  */ const Spacer(),
-
-            // name field
-            Text(user.name!),
-            // Padding(
-            //   padding: const EdgeInsets.all(Brand.padding),
-            //   child: TextField(
-            //     readOnly: true,
-            //     decoration: InputDecoration(
-            //       labelText: 'Name',
-            //       filled: true,
-            //       fillColor: Brand.containerBG,
-            //       border: OutlineInputBorder(
-            //         borderSide: BorderSide.none,
-            //         borderRadius: BorderRadius.circular(10),
-            //       ),
-            //       labelStyle: const TextStyle(
-            //         color: Brand.primary,
-            //       ),
-            //     ),
-            //   ),
-            // ),
-
-            // phone field
-            Padding(
-              padding: const EdgeInsets.all(Brand.padding),
-              child: TextField(
-                controller: TextEditingController(text: user.phone),
-                decoration: InputDecoration(
-                  labelText: 'Phone',
-                  filled: true,
-                  fillColor: Brand.containerBG,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  labelStyle: const TextStyle(
-                    color: Brand.primary,
-                  ),
-                ),
-              ),
+      body: SizedBox(width:double.infinity, child: Column(children: [
+        Expanded(child: Center(child: CircleAvatar(backgroundImage: AssetImage(user.picture), radius: 100),)),
+        Column(children: [
+          Container(margin: const EdgeInsets.all(10), decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(10)), color:Colors.grey), padding: const EdgeInsets.only(top: 12, bottom: 6), 
+            child: TextFormField(enabled: false, keyboardType: TextInputType.text, controller: TextEditingController(text: user.name), decoration: const InputDecoration(labelText: 'Nome', hintText: 'Entre com seu nome',
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent), // Borda invisível
             ),
-
-            // email field
-            Padding(
-              padding: const EdgeInsets.all(Brand.padding),
-              child: TextField(
-                controller: TextEditingController(text: user.email),
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  filled: true,
-                  fillColor: Brand.containerBG,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  labelStyle: const TextStyle(
-                    color: Brand.primary,
-                  ),
-                ),
-              ),
+          suffixIcon: Icon(Icons.edit)),)),
+          Container(margin: const EdgeInsets.all(10), decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(10)), color:Colors.grey), padding: const EdgeInsets.only(top: 12, bottom: 6), 
+          child: TextFormField(enabled: false, keyboardType: TextInputType.number, controller: TextEditingController(text: user.phone), decoration: const InputDecoration(labelText: 'Celular', hintText: 'Entre com seu número de celular',
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent), // Borda invisível
             ),
-          ],
-        ),
-      ),
+          suffixIcon: Icon(Icons.edit)),)),
+          Container(margin: const EdgeInsets.all(10), decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(10)), color:Colors.grey), padding: const EdgeInsets.only(top: 12, bottom: 6), 
+          child: TextFormField(enabled: false, keyboardType: TextInputType.emailAddress, controller: TextEditingController(text: user.email), decoration: const InputDecoration(labelText: 'E-mail', hintText: 'Entre com seu email',
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent), // Borda invisível
+            ),
+          suffixIcon: Icon(Icons.edit)),)),
+        ],)
+      ],),)
     );
+    // return Scaffold(
+    //   // backgroundColor: Brand.background,
+    //   appBar: const AppTitle('Perfil'),
+    //   body: Padding(
+    //     padding: const EdgeInsets.all(Brand.padding * 3),
+    //     child: Column(
+    //       children: [
+    //         // avatar
+    //         /*  */ const Spacer(),
+    //         Image.asset(user.picture),
+    //         /*  */ const Spacer(),
+
+    //         // name field
+    //         Text(user.name!),
+    //         // Padding(
+    //         //   padding: const EdgeInsets.all(Brand.padding),
+    //         //   child: TextField(
+    //         //     readOnly: true,
+    //         //     decoration: InputDecoration(
+    //         //       labelText: 'Name',
+    //         //       filled: true,
+    //         //       fillColor: Brand.containerBG,
+    //         //       border: OutlineInputBorder(
+    //         //         borderSide: BorderSide.none,
+    //         //         borderRadius: BorderRadius.circular(10),
+    //         //       ),
+    //         //       labelStyle: const TextStyle(
+    //         //         color: Brand.primary,
+    //         //       ),
+    //         //     ),
+    //         //   ),
+    //         // ),
+
+    //         // phone field
+    //         Padding(
+    //           padding: const EdgeInsets.all(Brand.padding),
+    //           child: TextField(
+    //             controller: TextEditingController(text: user.phone),
+    //             decoration: InputDecoration(
+    //               labelText: 'Phone',
+    //               filled: true,
+    //               fillColor: Brand.containerBG,
+    //               border: OutlineInputBorder(
+    //                 borderSide: BorderSide.none,
+    //                 borderRadius: BorderRadius.circular(10),
+    //               ),
+    //               labelStyle: const TextStyle(
+    //                 color: Brand.primary,
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+
+    //         // email field
+    //         Padding(
+    //           padding: const EdgeInsets.all(Brand.padding),
+    //           child: TextField(
+    //             controller: TextEditingController(text: user.email),
+    //             decoration: InputDecoration(
+    //               labelText: 'E-mail',
+    //               filled: true,
+    //               fillColor: Brand.containerBG,
+    //               border: OutlineInputBorder(
+    //                 borderSide: BorderSide.none,
+    //                 borderRadius: BorderRadius.circular(10),
+    //               ),
+    //               labelStyle: const TextStyle(
+    //                 color: Brand.primary,
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
